86437c031b35d9294c68f16e5f5bb233d5f838c6
Silvano-Paulino <silvanocavimbi20@gmail.com>
2024-08-25 17:07:23 +0100

    test: salary after late

--- cirt.go
+++ cirt.go
@@ -1,6 +1,6 @@
 package cirt
 
-type Service struct {}
+type Service struct{}
 
 func NewService() Service {
 	return Service{}
@@ -8,4 +8,9 @@ func NewService() Service {
 
 func (s Service) SalaryPerDay(salaryBase float64, days int) float64 {
 	return salaryBase / float64(days)
+}
+
+func (s Service) CalculateSalaryAfterLate(salaryBase float64, days, late int) float64 {
+	salaryPerDay := s.SalaryPerDay(salaryBase, days)
+	return salaryPerDay * float64(late)
 }
--- cirt_test.go
+++ cirt_test.go
@@ -26,6 +26,24 @@ func TestCirt(t *testing.T) {
 			}
 		})
 
+		t.Run("Should calculate salary base after late", func(t *testing.T) {
+			// Arrange
+			service := cirt.NewService()
+			salaryBase := 20000.0
+			days := 20
+			late := 2
+
+			expected := 2000
+
+			// Act
+			result := service.CalculateSalaryAfterLate(salaryBase, days, late)
+
+			// Assert
+			if result != float64(expected) {
+				t.Errorf("Expeted %v, received %v", expected, result)
+			}
+		})
+
 	})
 
 }
