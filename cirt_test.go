a5cb57fb167aa0349363e44c0e1476ca7e87a21d
Silvano-Paulino <silvanocavimbi20@gmail.com>
2024-08-25 16:53:08 +0100

    test: salary per day

--- cirt_test.go
+++ cirt_test.go
@@ -1,1 +1,31 @@
 package cirt_test
+
+import (
+	"testing"
+
+	cirt "github.com/silvano-paulino"
+)
+
+func TestCirt(t *testing.T) {
+	t.Run("Salary base after late", func(t *testing.T) {
+
+		t.Run("Should calculate salary base per day", func(t *testing.T) {
+			// Arrange
+			service := cirt.NewService()
+			salaryBase := 20000.0
+			days := 20
+
+			expeted := 1000
+
+			// Act
+			result := service.SalaryPerDay(salaryBase, days)
+
+			// Assert
+			if result != float64(expeted) {
+				t.Errorf("Expeted %v, received %v", expeted, result)
+			}
+		})
+
+	})
+
+}
